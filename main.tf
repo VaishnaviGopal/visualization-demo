fe085463090109a88f7bd9e50ca34ec7ad511eda
Sakshi Agarwal <sakshiag@in.ibm.com>
2020-12-10 17:00:42 +0530

    fix the issue in the repo

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "ibm_resource_instance" "cos_instance" {
 }
 
 resource "ibm_cos_bucket" "demo_bucket01" {
-  bucket_name          = "sch001_demo_bucket01"
+  bucket_name          = "schbucket01"
   resource_instance_id = ibm_resource_instance.cos_instance.id
   region_location      = "us-south"
   storage_class        = "standard"
@@ -70,7 +70,7 @@ resource "ibm_cos_bucket" "demo_bucket01" {
 }
 
 resource "ibm_cos_bucket" "demo_bucket02" {
-  bucket_name          = "sch002_demo_bucket02"
+  bucket_name          = "schbucket02"
   resource_instance_id = ibm_resource_instance.cos_instance.id
   region_location      = "us-south"
   storage_class        = "standard"
@@ -133,13 +133,13 @@ resource "ibm_iam_service_policy" "policy" {
 #**********************************************
 
 resource "ibm_is_vpc" "vpc1" {
-  name = "myvpc"
+  name = "myvpc10demo"
 }
 
 resource "ibm_is_subnet" "subnet1" {
   name                     = "mysubnet1"
   vpc                      = ibm_is_vpc.vpc1.id
-  zone                     = "us_south-1"
+  zone                     = "us-south-1"
   total_ipv4_address_count = 256
 }
 
@@ -153,10 +153,10 @@ resource "ibm_is_subnet" "subnet2" {
 resource "ibm_container_vpc_cluster" "cluster" {
   name              = "mycluster"
   vpc_id            = ibm_is_vpc.vpc1.id
-  flavor            = "bc1-2x8"
+  flavor            = "bx2.2x8"
   worker_count      = 3
   resource_group_id = ibm_resource_group.demo_group1.id
-
+  wait_till = "MasterNodeReady"
   zones {
     subnet_id = ibm_is_subnet.subnet1.id
     name      = "us-south-1"
@@ -168,10 +168,10 @@ resource "ibm_container_vpc_cluster" "cluster" {
   ]
 }
 
-resource "ibm_container_bind_service" "bind_service" {
-  cluster_name_id     = ibm_container_vpc_cluster.cluster.id
-  service_instance_id = element(split(":", ibm_resource_instance.cos_instance.id), 7)
-  namespace_id        = "default"
-  role                = "Writer"
-}
+#resource "ibm_container_bind_service" "bind_service" {
+ # cluster_name_id     = ibm_container_vpc_cluster.cluster.id
+#  service_instance_id = element(split(":", ibm_resource_instance.cos_instance.id), 7)
+#  namespace_id        = "default"
+#  role                = "Writer"
+#}
 
